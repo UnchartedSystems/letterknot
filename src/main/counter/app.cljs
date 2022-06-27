b4cc6b6e57bdda36a3fa899cf43a235b57ed995d
Dan Whitaker <dan@uncharted.systems>
2022-06-27 07:09:20 -0600

    reagent refresher

--- src/main/counter/app.cljs
+++ src/main/counter/app.cljs
@@ -4,12 +4,22 @@
 
 (println "Code Reloaded:")
 
-(defonce current-count (r/atom 0))
+(defonce counters (r/atom []))
+
+(defn add-counter [i]
+  (let [to-zero     (fn [& _] 0)
+        vec-delete  (fn [v i] (into (subvec v 0 i) (subvec v (inc i))))]
+    [:div {:key (str i)} (str (get @counters i))
+     [:button {:onClick #(swap! counters update i inc)} " +1 "]
+     [:button {:onClick #(swap! counters update i dec)} " -1 "]
+     [:button {:onClick #(swap! counters update i to-zero)} " Reset "]
+     [:button {:onClick #(swap! counters vec-delete i)} " X "]]))
 
 (defn application []
-  [:div @current-count " "
-   [:button {:onClick #(swap! current-count inc)} " + "]
-   [:button {:onClick #(swap! current-count dec)} " - "]])
+  [:div
+   [:h1 "Counter"]
+   [:button {:onClick #(swap! counters conj 0)} "New Counter"]
+   (doall (map add-counter (range (count @counters))))])
 
 (dom/render [application] (js/document.getElementById "app"))
 
